56f8b609ac9d20fcda80c2726843938b5bd9f550
Benno Fünfstück <benno.fuenfstueck@tu-dresden.de>
2021-06-04 15:48:48 +0200

    make build-graphviz independent of arch

--- tasks/build-graphviz.sh
+++ tasks/build-graphviz.sh
@@ -4,6 +4,6 @@ cd /tmp/
 wget https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/2.47.2/graphviz-2.47.2.tar.gz
 tar xf graphviz-2.47.2.tar.gz
 cd graphviz-2.47.2
-./configure --prefix=$OUT/graphviz.ppc64le --disable-perl --disable-java --disable-lua --enable-python3
+./configure --prefix=$OUT/graphviz.$(uname -i) --disable-perl --disable-java --disable-lua --enable-python3
 make -j8
 make install
